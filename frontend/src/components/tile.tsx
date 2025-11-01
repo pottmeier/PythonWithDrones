ebf80d5efff3b1d5358965eeef8ef73759d0a9f5
pottmeier <91335395+pottmeier@users.noreply.github.com>
2025-11-01 19:14:46 +0100

    Change tile model URL to GitHub link
    Updated the model URL for the Tile component to a GitHub link.

--- frontend/src/components/tile.tsx
+++ frontend/src/components/tile.tsx
@@ -4,7 +4,7 @@ import React, { useEffect } from "react";
 import { useGLTF } from "@react-three/drei";
 
 export default function Tile(props: any) {
-  const { scene } = useGLTF("/tile.glb");
+  const { scene } = useGLTF("https://github.com/pottmeier/PythonWithDrones/blob/main/frontend/public/tile.glb"); //TODO: Change this into variable model call
   const clonedScene = scene.clone();
 
   useEffect(() => {
@@ -20,4 +20,4 @@ export default function Tile(props: any) {
   );
 }
 
-useGLTF.preload("/tile.glb");
+useGLTF.preload("https://github.com/pottmeier/PythonWithDrones/blob/main/frontend/public/tile.glb"); //TODO: Change this into variable model call
