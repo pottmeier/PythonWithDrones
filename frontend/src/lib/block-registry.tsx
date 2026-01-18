1eaf24cf9a8883622d8d167f8341361fb8a3522a
stefanreitemeyer <stefan-reitemeyer@gmx.de>
2026-01-18 02:08:05 +0100

    added a finish block as a solve condition

--- frontend/src/lib/block-registry.tsx
+++ frontend/src/lib/block-registry.tsx
@@ -3,13 +3,15 @@ import GrassTile from "@/components/grass-tile";
 import DirtTile from "@/components/dirt-tile";
 import TrunkBlock from "@/components/trunk-block"; 
 import LeavesBlock from "@/components/leaves-block";
+import FinishPortalBlock from "@/components/finish-portal-block";
 
 // 1. Define the capabilities of a block
 export interface BlockDefinition {
   id: string;
   isCollidable: boolean;      // Can the drone fly through it?
-  isDestructible: boolean;    // Can it be drilled?
+  isDestructible: boolean;    // Can it be destroyed?
   isPickable: boolean;        // Can it be picked up?
+  isFinish?: boolean;         // Is the block a finish
   placementValidOn: string[]; // List of block IDs this can sit on top of
   component: React.ComponentType<any>; // The React component to render
 }
@@ -50,6 +52,17 @@ export const BLOCK_REGISTRY: Record<string, BlockDefinition> = {
     isPickable: false,
     placementValidOn: ["tree_trunk", "tree_leaves"],
     component: LeavesBlock,
+  },
+
+  //finish block
+  finish_portal: {
+    id: "finish_portal",
+    isCollidable: false, // Must be FALSE so we can fly into it
+    isFinish: true,      // <-- THE WIN TRIGGER
+    isDestructible: false,
+    isPickable: false,
+    placementValidOn: ["grass", "dirt", "empty"],
+    component: FinishPortalBlock,
   },
 
   // --- SPECIAL ---
